d982b13c4e7e8bb96858119ff064bee2331a1871
Dan_Imai <oinarhythm0508@gmail.com>
2017-01-13 23:41:41 +0900

    Initial Cmmit

---
+++ app/src/main/java/oren_bigmuff/com/database_rehab/MainActivity.java
@@ -0,0 +1,111 @@
+package oren_bigmuff.com.database_rehab;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private final static int WC = LinearLayout.LayoutParams.WRAP_CONTENT;
+    private final static String TAG_MESSAGE = "0";
+    private final static String TAG_YESNO = "1";
+    private final static String TAG_TEXT = "2";
+    private final static String TAG_IMAGE = "3";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Layoutの生成
+        LinearLayout layout = new LinearLayout(this);
+        layout.setBackgroundColor(Color.WHITE);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layout);
+        //Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Buttonの生成
+        layout.addView(makeButton("メッセージダイアログの表示", TAG_MESSAGE));
+        layout.addView(makeButton("Yes/Noダイアログの表示", TAG_YESNO));
+        layout.addView(makeButton("テキスト入力ダイアログの表示", TAG_TEXT));
+        layout.addView(makeButton(res2bmp(this, R.drawable.sample), TAG_IMAGE));
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public Bitmap res2bmp(Context context, int resID) {
+        return BitmapFactory.decodeResource(
+                context.getResources(),resID);
+    }
+
+    //ボタンの生成
+    private Button makeButton(String text, String tag) {
+        Button button = new Button(this);
+        button.setText(text);
+        button.setTag(tag);
+        button.setOnClickListener(this);
+        button.setLayoutParams(new LinearLayout.LayoutParams(WC, WC));
+        return button;
+    }
+
+    //イメージボタンの生成
+    private ImageButton makeButton(Bitmap bmp, String tag){
+        ImageButton button = new ImageButton(this);
+        button.setTag(tag);
+        button.setOnClickListener(this);
+        button.setImageBitmap(bmp);
+        button.setLayoutParams(new LinearLayout.LayoutParams(WC, WC));
+        return button;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
